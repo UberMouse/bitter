fb65f8f8c38fa0343b230e46bb0c75aa90c2a674
HSalmon & Genevieve Parkes <pair+HSalmon.genevievebelle@devbootcamp.com>
2014-06-05 11:40:24 +1200

    create registration, login directs to profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  def self.authenticate(user_name, password)
+    if User.exists?(user_name: user_name)
+      user = User.find_by_user_name(user_name)
+      if user.password == password
+        user
+      end
+    else
+      nil
+    end
+  end
 end
